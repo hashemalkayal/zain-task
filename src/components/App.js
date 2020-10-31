67d2594410e7c3daa64500cc118ce4ad949b27b3
Hashem Alkayal <hashemkaial9@gmail.com>
2020-10-31 14:48:37 +0200

    user Rename

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
 import "../sass/App.scss";
 import LoginPage from "./LoginPage";
 import ProtectedRoute from "./ProtectedRoute";

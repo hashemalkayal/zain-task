b380f7583a7c59f7f4454252e51689725447253b
Hashem Alkayal <hashemkaial9@gmail.com>
2020-10-31 15:16:29 +0200

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "../sass/Navbar.scss";
+import { Button, Dropdown, Menu } from "semantic-ui-react";
 
 function Navbar() {
-  return <div className="navbar-container">site navbar Here</div>;
+  return (
+    <div className="navbar-container">
+      <div className="navbar-controls">
+        <Menu size="large">
+          <Menu.Item icon="sidebar" active />
+          <Menu.Item name="Company Logo" />
+          <Menu.Menu position="right">
+            <Menu.Item icon="search" />
+            <Menu.Item icon="bell" />
+            <Menu.Item icon="calendar outline" />
+            <Menu.Item>
+              <Button icon="users" />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
